b6587912b29480296ea07c4c6fc938b161395a14
cpro95 <cpro95@users.noreply.github.com>
2019-08-15 18:25:20 +0800

    error fixed

--- src/routes/listAll.js
+++ src/routes/listAll.js
@@ -100,7 +100,7 @@ const ListAll = () => {
                 <td>
                   {d.slice(0,d.indexOf(search))}
                    <span className="text-white bg-dark">{search}</span>
-                  {d.slice(d.indexOf(search))}
+                  {d.slice(d.indexOf(search)+1)}
                   <Link
                     to={{
                       pathname: `${i}`,
